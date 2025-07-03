237466817e8c82971e0f4848dc667429bb70b47b
Paras Kalyan <paraskalyan20@gmail.com>
2025-07-03 18:13:56 +0530

    Template added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+function App() {
+
+  return (
+    <div className='min-h-screen flex flex-col gap-3 bg-black items-center justify-center'>
+      <h1 className='text-white text-3xl font-bold'>Shadcn JavaScript Template</h1>
+      <h6 className='text-white'>By Paras Kalyan</h6>
+      <Button>Click Me!</Button>
+      <img src='/vite.svg' />
+    </div>
+  )
+}
+
+export default App

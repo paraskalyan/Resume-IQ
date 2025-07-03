dc234439991646d9a1980cdef2ed81c7c74fd601
Paras Kalyan <paraskalyan20@gmail.com>
2025-07-03 18:14:54 +0530

    Template added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className='min-h-screen flex flex-col gap-3 bg-black items-center justify-center'>
       <h1 className='text-white text-3xl font-bold'>Shadcn JavaScript Template</h1>
       <h6 className='text-white'>By Paras Kalyan</h6>
-      <Button>Click Me!</Button>
+      <Button>Click Me</Button>
       <img src='/vite.svg' />
     </div>
   )
